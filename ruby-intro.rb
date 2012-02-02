669d5cfa0e4a80e9bf93471c67d7e95c02ae8a08
Michael Ladt <michaelladt@gmail.com>
2012-02-02 11:37:47 -0700

    ruby-intro.rb

---
+++ ruby-intro.rb
@@ -0,0 +1,27 @@
+class ASSIGNMENT
+
+  def ASSIGNMENT.showtime
+    puts Time.new
+  end
+showtime
+
+
+  def ASSIGNMENT.thousand
+    (1..1000).each { |x| puts "im on #{x}" }
+  end
+thousand
+
+  def ASSIGNMENT.addtwo(a,b)
+    a+b
+  end
+
+  s = "one two three four"
+    s.scan("one")
+  end
+
+  s = "one two three four"
+    s.include?("two")
+  end
+
+end
+
